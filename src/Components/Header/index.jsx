f469b513a1ee9962437f2555a1a7149916ad1f22
Kass <dongduclan27072001@gmail.com>
2025-03-05 13:41:05 +0700

    commit

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -36,7 +36,7 @@ const Index = () => {
     }
     const isIOS = /iPhone|iPad|iPod/.test(navigator.userAgent);
     const isAndroid = /Android/.test(navigator.userAgent);
-    const zaloLink = isIOS ? 'zalo://qr/p/oy7hpunuybc0' : isAndroid ? 'https://zaloapp.com/qr/p/oy7hpunuybc0' : 'zalo://conversation?phone=0928794999';
+    const zaloLink = isIOS ? 'zalo://qr/p/1n5nuejq36yqe' : isAndroid ? 'https://zaloapp.com/qr/p/1n5nuejq36yqe' : 'zalo://conversation?phone=0975480012';
     return (
         <>
             <div className={isIOS ? 'hotline-icon-one-ios' : isAndroid ? 'hotline-icon-one-android' : 'hotline-icon-one-pc'}>
